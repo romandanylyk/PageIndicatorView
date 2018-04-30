d9b1f8b038c59abd24113dff39359d81e9d9e5c1
romandanylyk <romandanylyk96@gmail.com>
2018-04-30 21:36:45 +0300

    Updated gradle file Code clean up

--- sample/src/main/java/com/rd/pageindicatorview/home/HomeActivity.java
+++ sample/src/main/java/com/rd/pageindicatorview/home/HomeActivity.java
@@ -1,7 +1,5 @@
 package com.rd.pageindicatorview.home;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -70,12 +68,6 @@ public class HomeActivity extends BaseActivity {
         pager.setAdapter(adapter);
 
         pageIndicatorView = findViewById(R.id.pageIndicatorView);
-        findViewById(R.id.btnOpenFragment).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				openFragment();
-			}
-		});
     }
 
     @NonNull
@@ -107,13 +99,5 @@ public class HomeActivity extends BaseActivity {
         pageIndicatorView.setRtlMode(customization.getRtlMode());
         pageIndicatorView.setInteractiveAnimation(customization.isInteractiveAnimation());
         pageIndicatorView.setAutoVisibility(customization.isAutoVisibility());
-    }
-
-    private void openFragment() {
-		FragmentManager manager = getFragmentManager();
-		FragmentTransaction transaction = manager.beginTransaction();
-		transaction.add(R.id.fragmentHolder, HomeFragment.getInstance(), "TAG");
-		transaction.addToBackStack(null);
-		transaction.commit();
     }
 }
